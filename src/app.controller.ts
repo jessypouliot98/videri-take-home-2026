37933e0e11ab8f0e1fbf9c60c470f022f977d819
Jessy Pouliot <jessypouliot98@gmail.com>
2026-03-06 17:15:23 -0500

    Revert "remove unused app service"
    This reverts commit 230d12968dd4a46d7cb8043da6ab79fccf92347a.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,19 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AppService } from './app.service.js';
 import { AuthGuard } from '../lib/nest/auth/auth.guard.js';
 import { Public } from '../lib/nest/auth/auth.decorator.js';
 
 @Controller()
 @UseGuards(AuthGuard)
 export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @Public()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
   @Get('health')
   @Public()
   getHealth(): string {
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,20 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UsersController } from './users.controller.js';
 import { UsersService } from './users.service.js';
 
-describe('UsersController', () => {
-  let controller: UsersController;
+describe('UsersService', () => {
+  let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [UsersController],
       providers: [UsersService],
     }).compile();
 
-    controller = module.get<UsersController>(UsersController);
+    service = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(service).toBeDefined();
   });
 });
