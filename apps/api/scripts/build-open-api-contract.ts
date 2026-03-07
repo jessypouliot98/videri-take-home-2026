34dfe2f137cfa83efffae5ff98b53d4dd8459cf9
Jessy Pouliot <jessypouliot98@gmail.com>
2026-03-06 21:46:35 -0500

    init nextjs + openapi codegen (not fully working)

--- apps/api/scripts/build-open-api-contract.ts
+++ apps/api/scripts/build-open-api-contract.ts
@@ -4,11 +4,8 @@ import { createApp, createOpenApiDocument } from '../src/app.js';
 async function generate() {
   const app = await createApp();
   const document = createOpenApiDocument(app);
-
-  fs.writeFileSync(
-    process.argv[2] ?? 'openapi.json',
-    JSON.stringify(document, null, 2),
-  );
+  const documentString = JSON.stringify(document, null, 2);
+  fs.writeFileSync(process.argv[2] ?? 'openapi.json', documentString);
 
   await app.close();
 }
